da1eef65d8c74af4bd7eff20886838115c684531
Davit <davit.ners02@gmail.com>
2025-11-27 19:31:24 +0100

    Add about page + image

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+import styles from "./about.module.css";
+import AboutHero from "../components/about/aboutHero/aboutHero";
+import AboutColumns from "../components/about/aboutColumns/aboutColumns";
+import AboutPanels from "../components/about/aboutPanels/aboutPanels";
+import AboutQuote from "../components/about/aboutQuote/aboutQuote";
+
+export default function AboutPage() {
+    return (
+        <div className={styles.page}>
+        <AboutHero />
+        <AboutColumns />
+        <AboutPanels />
+        <AboutQuote text="« La peinture n’est pas ce que l’on voit, mais ce que l’on ressent. »" />
+        </div>
+    );
+};
