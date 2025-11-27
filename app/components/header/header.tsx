3738a23c2c2a8d7b0ea179cc96199305bc815ba7
Davit <davit.ners02@gmail.com>
2025-11-27 15:32:38 +0100

    Add header component

---
+++ app/components/header/header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import styles from "./header.module.css";
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+        <div className={styles.container}>
+            <div className={styles.logo}>Pino Onali</div>
+
+            <nav className={styles.nav}>
+            <Link href="/">Accueil</Link>
+            <Link href="/galerie">Galerie</Link>
+            <Link href="/a-propos">Á Propos</Link>
+            <Link href="/contact">Contact</Link>
+            </nav>
+        </div>
+        </header>
+    );
+};
