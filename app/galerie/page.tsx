46944500ce25ccfc503c192a45fde1cdd22a24bb
Davit <davit.ners02@gmail.com>
2025-11-27 16:40:04 +0100

    Add gallerie page

---
+++ app/galerie/page.tsx
@@ -0,0 +1,55 @@
+import ArtworkGrid from "../components/gallery/artworkGrid/artworkGrid";
+
+export type Artwork = {
+    id: string;
+    title: string;
+    description?: string;
+    imageUrl: string;
+};
+
+const artworks: Artwork[] = [
+    {
+        id: "1",
+        title: "Constellations intérieures",
+        description: "Acrylique sur toile – 80 x 100 cm",
+        imageUrl: "/artworks/ex1.jpg",
+    },
+    {
+        id: "2",
+        title: "Lune rouge",
+        description: "Huile sur toile – 60 x 80 cm",
+        imageUrl: "/artworks/ex2.jpg",
+    },
+    {
+        id: "3",
+        title: "Éclats d’astres",
+        description: "Techniques mixtes – 100 x 120 cm",
+        imageUrl: "/artworks/ex3.jpg",
+    },
+    {
+        id: "4",
+        title: "Constellations intérieures",
+        description: "Acrylique sur toile – 80 x 100 cm",
+        imageUrl: "/artworks/ex1.jpg",
+    },
+    {
+        id: "5",
+        title: "Lune rouge",
+        description: "Huile sur toile – 60 x 80 cm",
+        imageUrl: "/artworks/ex2.jpg",
+    },
+    {
+        id: "6",
+        title: "Éclats d’astres",
+        description: "Techniques mixtes – 100 x 120 cm",
+        imageUrl: "/artworks/ex3.jpg",
+    },
+];
+
+export default function GaleriePage() {
+    return (
+        <section>
+            <ArtworkGrid artworks={artworks} />
+        </section>
+    );
+};
